09cc43c7abfedb84ea22a73585e77df1e64fb05a
Emerson Millard <emillard.code@gmail.com>
2022-07-28 12:00:00 -0700

    Modified test class.

--- src/test/java/com/project/springcloudfeignjt/SpringCloudFeignJtApplicationTests.java
+++ src/test/java/com/project/springcloudfeignjt/SpringCloudFeignJtApplicationTests.java
@@ -1,13 +1,8 @@
 package com.project.springcloudfeignjt;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCloudFeignJtApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
