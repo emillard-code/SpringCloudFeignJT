da06200639331403db3877d755366c6bbd193f2f
Emerson Millard <emillard.code@gmail.com>
2022-07-28 12:00:00 -0700

    Modified main class.

---
+++ src/main/java/com/project/SpringCloudFeignJtApplication.java
@@ -0,0 +1,33 @@
+package com.project;
+
+import com.project.client.UserClient;
+import com.project.dto.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@EnableFeignClients
+public class SpringCloudFeignJtApplication {
+
+    @Autowired
+    private UserClient client;
+
+    @GetMapping("/findAllUser")
+    public List<UserResponse> getAllUser() {
+        return client.getUsers();
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringCloudFeignApplication.class, args);
+
+    }
+
+}
