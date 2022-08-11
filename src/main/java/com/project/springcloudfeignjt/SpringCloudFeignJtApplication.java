026c7058f03e3eca32ac0b742206e8f7d23afbac
Emerson Millard <emillard.code@gmail.com>
2022-08-11 16:34:02 -0700

    Initial commit

---
+++ src/main/java/com/project/springcloudfeignjt/SpringCloudFeignJtApplication.java
@@ -0,0 +1,13 @@
+package com.project.springcloudfeignjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFeignJtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFeignJtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/springcloudfeignjt/SpringCloudFeignJtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springcloudfeignjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudFeignJtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
